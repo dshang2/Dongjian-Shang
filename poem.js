ecd79b326ea21353aa44f1a71591c56770b8ea57
dshang2 <dsshang2@gmail.com>
2022-05-02 12:34:44 -0400

    1

--- poem.js
+++ poem.js
@@ -10,42 +10,36 @@ $(document).ready(function () {
   $("#name").change(function () {
     //Creates a variable with the value of the #name form field.
     var name = $("#name").val();
-    //Sets the text of any <h1> on the page to "Hello name!". Automatically updated when the form field is change and you leave the field.
     $("h1").text("I, " + name + ",");
   });
 
   $("#Adjectives").change(function () {
     //Creates a variable with the value of the #name form field.
     var Adjectives = $("#Adjectives").val();
-    //Sets the text of any <h1> on the page to "Hello name!". Automatically updated when the form field is change and you leave the field.
     $("h2").text(" " + Adjectives + ",");
   });
 
   $("#Lover").change(function () {
     //Creates a variable with the value of the #name form field.
     var Adjectives = $("#Lover").val();
-    //Sets the text of any <h1> on the page to "Hello name!". Automatically updated when the form field is change and you leave the field.
     $("h3").text("Lover of  " + Adjectives + ",");
   });
 
   $("#Feeling").change(function () {
     //Creates a variable with the value of the #name form field.
     var Feeling = $("#Feeling").val();
-    //Sets the text of any <h1> on the page to "Hello name!". Automatically updated when the form field is change and you leave the field.
     $("h4").text("Who feels " + Feeling + ",");
   });
 
   $("#See").change(function () {
     //Creates a variable with the value of the #name form field.
     var See = $("#See").val();
-    //Sets the text of any <h1> on the page to "Hello name!". Automatically updated when the form field is change and you leave the field.
     $("h5").text("Who would like to see " + See + ",");
   });
 
   $("#City").change(function () {
     //Creates a variable with the value of the #name form field.
     var City = $("#City").val();
-    //Sets the text of any <h1> on the page to "Hello name!". Automatically updated when the form field is change and you leave the field.
     $("h6").text(
       "Am a FUTURE resident of " + City + ", Welcome you to my poem."
     );
